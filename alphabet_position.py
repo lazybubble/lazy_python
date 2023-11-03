c1aef197c64eee6aff4057adb8696880038e7de9
lazybubble <118935410+lazybubble@users.noreply.github.com>
2023-11-03 23:45:48 +0100

    Create alphabet_position.py

---
+++ alphabet_position.py
@@ -0,0 +1,16 @@
+import strings 
+def alphabet_position(text):
+    pass
+listed = list(text)
+if str == letters:
+    for char in listed:
+        print(''join.(my_list[char] = index + 1))
+else 
+    pass
+
+textie = 'Hello beautiful world!'
+alphabet_position(textie)
+
+
+   #  make it into a list, slice it so that it can be easily replaced by the value in the dict
+    #    position = 
