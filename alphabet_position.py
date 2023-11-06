62e6cb70316e07edff73901a5c86f23cbab062aa
lazybubble <118935410+lazybubble@users.noreply.github.com>
2023-11-06 23:48:54 +0100

    final submission alphabet_position.py

--- alphabet_position.py
+++ alphabet_position.py
@@ -14,3 +14,28 @@ alphabet_position(textie)
 
    #  make it into a list, slice it so that it can be easily replaced by the value in the dict
     #    position = 
+##Final solution submitted
+
+#1. create dict with alphabet and indexes + 1
+#2. convert input into str so they are iterable
+#3. iterate the input through a loop that will return values
+#4. create loop that returns error message
+
+import string
+
+def alphabet_position(text):
+    str_text = str(text)
+    
+    alphabet = string.ascii_lowercase
+    alph_dict = {letters:str(index + 1)} for index, letter in enumerate
+    
+    text = str_text.lower
+    result = [alpha_dict[char] for char in text if char.isalpha()]
+    
+    if not result:
+        return 'There are no alphabetic characters in the input.'
+    return ''.join(result)
+    
+textie = 'hello beautiful world!'
+positions = alphabet_position(textie)
+print(positions)
